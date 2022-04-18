bca40b61cdeed9a1c5ebcbdc1a91c72759fa8acc
Mateusz <echelon133@gmail.com>
2022-04-18 11:41:42 +0200

    Fix bitboard docs

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -12,8 +12,14 @@ use crate::square;
 /// - 'h8' is 0x8000000000000000 (only the most significant bit)
 ///
 /// The ordering goes left-to-right and bottom-to-top. [`square::Square`] holds
-/// its index (in range 0..=63) which is basically the value of bits that have to 
-/// be shifted right to get to the bit that represents that square.
+/// its index (in range 0..=63) which is basically the number of bits of the bitboard
+/// that have to be shifted right to get to the bit that represents that square.
+///
+/// If we want to get the value of the bit that represents the 'b1' square,
+/// we get the index of that square, which is 1. This means that to get the bit
+/// that represents that square we shift bitboard's bits to the right once
+/// and then we check whether the least significant bit after that shift is
+/// equal to 1. 
 ///
 #[derive(Copy, Clone)]
 pub struct Bitboard {
