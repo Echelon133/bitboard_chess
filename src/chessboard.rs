885606468be666d6f2f6085b7071e102690a987b
Mateusz <echelon133@gmail.com>
2022-05-22 20:26:16 +0200

    Move constants from chessboard module to another module

--- src/chessboard.rs
+++ src/chessboard.rs
@@ -7,64 +7,7 @@ use crate::movegen;
 use crate::moves;
 use crate::piece;
 use crate::square;
-
-/// Squares that create a path between the white king and H1 rook,
-/// which cannot be attacked by a black piece if the player wants to
-/// legally castle.
-const WHITE_KINGSIDE_CASTLING_PATH: [square::Square; 3] = [
-    square::Square::new(square::Rank::R1, square::File::E),
-    square::Square::new(square::Rank::R1, square::File::F),
-    square::Square::new(square::Rank::R1, square::File::G),
-];
-
-/// Squares that create a path between the black king and H8 rook,
-/// which cannot be attacked by a white piece if the player wants to
-/// legally castle.
-const BLACK_KINGSIDE_CASTLING_PATH: [square::Square; 3] = [
-    square::Square::new(square::Rank::R8, square::File::E),
-    square::Square::new(square::Rank::R8, square::File::F),
-    square::Square::new(square::Rank::R8, square::File::G),
-];
-
-/// Squares that create a path between the white king and A1 rook,
-/// which cannot be attacked by a black piece if the player wants to
-/// legally castle.
-const WHITE_QUEENSIDE_CASTLING_PATH: [square::Square; 3] = [
-    square::Square::new(square::Rank::R1, square::File::C),
-    square::Square::new(square::Rank::R1, square::File::D),
-    square::Square::new(square::Rank::R1, square::File::E),
-];
-
-/// Squares that create a path between the black king and A8 rook,
-/// which cannot be attacked by a white piece if the player wants to
-/// legally castle.
-const BLACK_QUEENSIDE_CASTLING_PATH: [square::Square; 3] = [
-    square::Square::new(square::Rank::R8, square::File::C),
-    square::Square::new(square::Rank::R8, square::File::D),
-    square::Square::new(square::Rank::R8, square::File::E),
-];
-
-/// Initial square of the white king.
-const WHITE_KING_START: square::Square = square::Square::new(square::Rank::R1, square::File::E);
-
-/// Initial square of the black king.
-const BLACK_KING_START: square::Square = square::Square::new(square::Rank::R8, square::File::E);
-
-/// Initial square of the white rook (queenside).
-const WHITE_QSIDE_ROOK_START: square::Square =
-    square::Square::new(square::Rank::R1, square::File::A);
-
-/// Initial square of the black rook (queenside).
-const BLACK_QSIDE_ROOK_START: square::Square =
-    square::Square::new(square::Rank::R8, square::File::A);
-
-/// Initial square of the white rook (kingside).
-const WHITE_KSIDE_ROOK_START: square::Square =
-    square::Square::new(square::Rank::R1, square::File::H);
-
-/// Initial square of the black rook (kingside).
-const BLACK_KSIDE_ROOK_START: square::Square =
-    square::Square::new(square::Rank::R8, square::File::H);
+use crate::chessboard_constants::*;
 
 /// Iterator over [`movegen::MoveIter`] iterators. There is a single [`movegen::MoveIter`] for
 /// every single square that's occupied by the player who is about to make a move.
