1080ed70851ed7921a1002708259f32f6d3fcd8d
Mateusz <echelon133@gmail.com>
2022-06-11 09:23:13 +0200

    Configure which modules should be public

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
-pub mod bitboard;
-pub mod board;
-pub mod chessboard;
-pub mod chessboard_constants;
-pub mod context;
-pub mod movegen;
-pub mod movegen_constants;
-pub mod moves;
-pub mod piece;
-pub mod square;
+//! This crate contains a chessboard implementation that's built using bitboards.
+//!
+//! Some of functionalities implemented by this crate:
+//! * creation of chessboards from FEN strings (with validation)
+//! * serialization of chessboards into FEN strings
+//! * execution of moves (represented in the UCI format)
+//! * rejection of moves that are invalid for the current state of the board
+//! * iteration of all legal moves that are possible in a given position
+//! * possibility of ending a game prematurely (propose draw or resign)
 
+mod bitboard;
+mod board;
+pub mod chessboard;
+mod chessboard_constants;
+mod context;
+mod movegen;
+mod movegen_constants;
+pub mod moves;
+mod piece;
+mod square;
